bafb8e7be1b7ec8b16d44d1ea6e16189a3c1482b
DiegoAguilera-PT5 <diegonicolasaguilerafrias@gmail.com>
2023-04-18 02:21:39 -0300

    Reformateo de codigo y responsive

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strick";
 
+const etapa = ["se encuentra en planta de distribución","se encuentra en camino al destinatario",
+"se encuentra listo para su retiro en nuestra estacion","se encuentra retenido en aduana",
+"ha sido cancelado por una la falta de una direccion valida"];
 const form = document.querySelector('.busqueda');
 const input = document.querySelector('.form');
 
@@ -14,7 +17,7 @@ form.addEventListener('submit', (event) => {
   // Verificamos si el código está dentro del rango válido
   if (codigo >= 0 && codigo <= 50000) {
     // Si está dentro del rango, mostramos un mensaje indicando que el paquete se encuentra en planta de distribución
-    alert(`El paquete con código ${codigo} se encuentra en planta de distribución`);
+    alert(`El paquete con código ${codigo} ` + etapa[Math.floor(Math.random() * etapa.length)]);
   } else {
     // Si no está dentro del rango, mostramos un mensaje indicando que no hay ningún paquete con ese código
     alert(`No se ha encontrado ningún paquete con el código ${codigo}`);
